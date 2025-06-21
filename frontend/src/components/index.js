da1b1ab52801d80362b20543574d1f723b6a140f
jlturner5597 <jlturner5597@gmail.com>
2025-06-21 17:41:08 -0400

    Add reusable components and integrate into App

---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './Header.jsx'
+export { default as Footer } from './Footer.jsx'
+export { default as PrimaryButton } from './PrimaryButton.jsx'
+export { default as DataTable } from './DataTable.jsx'
